3b341ec4fbae2f11e7abd1afd0522ec555047f12
Florian Beaufumé <fbeaufume@gmail.com>
2022-11-22 18:50:53 +0100

    Added an empty Spring Boot application

---
+++ src/main/java/com/adeliosys/sample/HibernateMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.adeliosys.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateMonitoringApplication.class, args);
+    }
+}
