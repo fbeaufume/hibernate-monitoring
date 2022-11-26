8c881938c0006488716342870b4bebc4e50559c3
Florian Beaufumé <fbeaufume@gmail.com>
2022-11-26 16:02:51 +0100

    Added author endpoints

--- src/main/java/com/adeliosys/sample/LibraryController.java
+++ src/main/java/com/adeliosys/sample/LibraryController.java
@@ -7,7 +7,11 @@ import com.adeliosys.sample.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Transactional
@@ -22,7 +26,25 @@ public class LibraryController {
     @GetMapping("/reset")
     public void reset() {
         authorRepository.deleteAll();
-        authorRepository.save(new Author("Author 1")
-                .addBooks(new Book("Book 1"), new Book()));
+        authorRepository.saveAll(List.of(
+                new Author("Author A").addBooks(
+                        new Book("Book A1")),
+                new Author("Author B").addBooks(
+                        new Book("Book B1"),
+                        new Book("Book B2"))));
+    }
+
+    @GetMapping("/list-authors")
+    public List<Long> listAuthors() {
+        return extractIds(authorRepository.findAll());
+    }
+
+    @GetMapping("/find-author")
+    public List<Long> findAuthor(@RequestParam String name) {
+        return extractIds(authorRepository.findByName(name));
+    }
+
+    public List<Long> extractIds(List<Author> authors) {
+        return authors.stream().map(Author::getId).collect(Collectors.toList());
     }
 }
--- src/main/java/com/adeliosys/sample/repository/AuthorRepository.java
+++ src/main/java/com/adeliosys/sample/repository/AuthorRepository.java
@@ -3,5 +3,9 @@ package com.adeliosys.sample.repository;
 import com.adeliosys.sample.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    List<Author> findByName(String name);
 }
